27408b3c5da88770f2f8094296af9cc5293bae20
RamyZayed <rami1112@windowslive.com>
2021-08-10 16:23:40 +0300

    log4j2.xml isn't working still

--- src/main/java/com/example/actualproject/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/actualproject/service/EmployeeServiceImpl.java
@@ -7,6 +7,10 @@ import com.example.actualproject.entity.dto.EmployeeDtoMapper;
 import com.example.actualproject.entity.dto.PersonAddressMapper;
 import com.example.actualproject.entity.dto.PersonAdressDTO;
 import com.example.actualproject.repository.EmployeeRepository;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,14 +21,18 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
 public class EmployeeServiceImpl  implements EmployeeService{
+
+    private static final Logger logger = LogManager.getLogger(EmployeeServiceImpl.class);
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
     public List<EmployeeDto> getAllEmployees(int page, int size){
 
+        logger.error("Trying to find all employees with page = "+page +"and size = "+size);
         Pageable result= PageRequest.of(page,size);
         return EmployeeDtoMapper.Instance.toDtoList(employeeRepository.findAll(result).toList());
 
@@ -37,6 +45,7 @@ public class EmployeeServiceImpl  implements EmployeeService{
 
     @Override
     public EmployeeDto findById(int id) {
+        logger.trace("trying to find person with id = "+id);
         EmployeeDto someone = EmployeeDtoMapper.Instance.toDto(employeeRepository.findById(id).get());
         return someone;
     }
@@ -45,18 +54,21 @@ public class EmployeeServiceImpl  implements EmployeeService{
     @Override
     @Transactional
     public Employee addEmployee(Employee employee) {
+        logger.trace("trying to add new employee: "+employee.toString());
         employeeRepository.save(employee);
         return employee;
     }
 
     @Override
     public Employee updateEmployee(Employee employee) {
+        logger.trace("trying to update  employee: "+employee.toString());
         employeeRepository.updatename(employee.getName(), employee.getId());
         return employeeRepository.findById(employee.getId()).get();
     }
 
     @Override
     public void deleteEmployeeById(int id) {
+        logger.trace("trying to delete an employee with id: "+id);
         employeeRepository.deleteById(id);
 
     }
--- src/main/java/com/example/actualproject/service/PersonServiceImpl.java
+++ src/main/java/com/example/actualproject/service/PersonServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.actualproject.service;
 
+import com.example.actualproject.entity.Address;
 import com.example.actualproject.entity.Employee;
 import com.example.actualproject.entity.Person;
 import com.example.actualproject.entity.dto.PersonAddressMapper;
 import com.example.actualproject.entity.dto.PersonAdressDTO;
 import com.example.actualproject.repository.PersonRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -12,41 +15,48 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PersonServiceImpl implements PersonService{
 
+    Logger logger = LoggerFactory.getLogger(PersonServiceImpl.class);
     @Autowired
     PersonRepository personRepository;
 
     @Override
-    public Person create(Person person) {
+    public Person create( Person person) {
+        logger.trace("created a person is accessed" +person.toString());
         return personRepository.save(person);
     }
 
     @Override
     public Person update(Person person , int id) {
          personRepository.updatename(person.getName(),id);
+        logger.trace("trying update Person with id : " +id +"    With Values :"  +person.toString() );
          return personRepository.findById(id).get();
     }
 
 
     @Override
     public List<PersonAdressDTO> get(int page  , int size) {
+        logger.info("accesing the service of getting all the people with page number of:"+page +"and page size of :"+size);
         Pageable result= PageRequest.of(page,size);
-        return  PersonAddressMapper.Instance.toDtoList(personRepository.findAll(result).toList()      ) ;
+        List<Address> addresses =new ArrayList<>();
+        return  PersonAddressMapper.Instance.toDtoList(personRepository.findAll(result).toList() ) ;
     }
 
     @Override
     public PersonAdressDTO findPersonById(int id) {
-
+        logger.trace("Trying to find Person with id = " +id);
         return  PersonAddressMapper.Instance.toDto(personRepository.findById(id).get(),personRepository.findById(id).get().getAddress());
     }
 
     @Override
     public void Delete(int id) {
+        logger.trace("Trying to delete person with id=" +id);
         personRepository.deleteById(id);
 
     }
